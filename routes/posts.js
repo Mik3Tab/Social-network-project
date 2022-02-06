4284fe71fa5800692d99b52339ee21e667402ac1
miguel Talavera <miguel.talavera1995@gmail.com>
2022-02-06 21:06:38 +0100

    Readme, controllers and routes updated

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,9 @@ const PostController = require('../controllers/PostController');
 const {authentication} = require("../middlewares/authentication");
 
 router.post('/create',authentication, PostController.create);
-router.get('/getByUsername', PostController.getPostByUsername);
+router.get('/getByUsername/:name', PostController.getPostByUsername);
 router.delete('/:_id',authentication, PostController.delete);
+router.get('/getById/:id', PostController.getById);
+router.post('/insert',PostController.insertComment);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
-router.post('/register', UserController.create);
-
+router.post('/register', UserController.register);
+router.get('/getById/:id', UserController.getById);
+router.delete('/castigao/:id',UserController.delete);
+router.post('/login', UserController.login);
 
 module.exports = router;
